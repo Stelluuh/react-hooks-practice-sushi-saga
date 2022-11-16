5fc2d277d877a54d04d0d12df34d173f19f7964f
stella <sftamesis13@gmail.com>
2022-11-16 11:23:18 -0800

    build heirarchy and fetch data from server using useEffect and useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushis, setSushis] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+    //fetch takes in a URL as its argument and makes an HTTP request/network request and returns a promise object that represents the data source sent back.
+      .then(response => response.json()) //.then is called on the promise object returned. It takes a callback function. This CB function uses json() method to convert the promise object into plain text in order to use in the next .then(). in return we also get a Promise.
+      .then(data => setSushis(data)) //this then() method is receiving the object that we returned from the first call to then() (our parsed JSON object). We capture the object in the paramater data and pass it into another CB function to manipulate the DOM.
+  }, []) // adding [] will only run the fetch once.
+
+  console.log(sushis)
+
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,7 +1,10 @@
 import React from "react";
 import MoreButton from "./MoreButton";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis }) {
+
+
+
   return (
     <div className="belt">
       {/* Render Sushi components here! */}
